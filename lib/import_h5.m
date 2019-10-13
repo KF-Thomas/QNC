abace197506f1d26320c13f83e37513cd4774fd4
KF-Thomas <35283813+KF-Thomas@users.noreply.github.com>
2019-10-13 19:23:54 +1100

    fixing matlab etc

--- lib/import_h5.m
+++ lib/import_h5.m
@@ -1,4 +1,43 @@
-function import_h5(file_name,directory)
-cd(directory)
-file_name
+function data = import_h5(file_name,directory)
+full_path = [directory,'/',file_name,'.h5'];
+try
+  data.t_1 = hdf5read(full_path, '/1/t');
+  data.x_1 = hdf5read(full_path, '/1/x');
+  data.pos_dens_1 = hdf5read(full_path, '/1/pos_dens');
+  data.pos_dens_1 = permute(data.pos_dens_1, ndims(data.pos_dens_1):-1:1);
+  data.pos_phase_1 = hdf5read(full_path, '/1/pos_phase');
+  data.pos_phase_1 = permute(data.pos_phase_1, ndims(data.pos_phase_1):-1:1);
+  data.t_2 = hdf5read(full_path, '/2/t');
+  data.nx_2 = hdf5read(full_path, '/2/nx');
+  data.herm_dens_2 = hdf5read(full_path, '/2/herm_dens');
+  data.herm_dens_2 = permute(data.herm_dens_2, ndims(data.herm_dens_2):-1:1);
+  data.herm_phase_2 = hdf5read(full_path, '/2/herm_phase');
+  data.herm_phase_2 = permute(data.herm_phase_2, ndims(data.herm_phase_2):-1:1);
+  data.t_3 = hdf5read(full_path, '/3/t');
+  data.kx_3 = hdf5read(full_path, '/3/kx');
+  data.mom_dens_3 = hdf5read(full_path, '/3/mom_dens');
+  data.mom_dens_3 = permute(data.mom_dens_3, ndims(data.mom_dens_3):-1:1);
+  data.mom_phase_3 = hdf5read(full_path, '/3/mom_phase');
+  data.mom_phase_3 = permute(data.mom_phase_3, ndims(data.mom_phase_3):-1:1);
+catch
+  data.t_1 = hdf5read(full_path, '/1/t');
+  data.x_1 = hdf5read(full_path, '/1/x');
+  data.pos_dens_1 = hdf5read(full_path, '/1/mean_pos_dens');
+  data.pos_dens_1 = permute(data.pos_dens_1, ndims(data.pos_dens_1):-1:1);
+  data.pos_phase_1 = hdf5read(full_path, '/1/mean_pos_phase');
+  data.pos_phase_1 = permute(data.pos_phase_1, ndims(data.pos_phase_1):-1:1);
+  data.t_2 = hdf5read(full_path, '/2/t');
+  data.nx_2 = hdf5read(full_path, '/2/nx');
+  data.herm_dens_2 = hdf5read(full_path, '/2/mean_herm_dens');
+  data.herm_dens_2 = permute(data.herm_dens_2, ndims(data.herm_dens_2):-1:1);
+  data.herm_phase_2 = hdf5read(full_path, '/2/mean_herm_phase');
+  data.herm_phase_2 = permute(data.herm_phase_2, ndims(data.herm_phase_2):-1:1);
+  data.t_3 = hdf5read(full_path, '/3/t');
+  data.kx_3 = hdf5read(full_path, '/3/kx');
+  data.mom_dens_3 = hdf5read(full_path, '/3/mean_mom_dens');
+  data.mom_dens_3 = permute(data.mom_dens_3, ndims(data.mom_dens_3):-1:1);
+  data.mom_phase_3 = hdf5read(full_path, '/3/mean_mom_phase');
+  data.mom_phase_3 = permute(data.mom_phase_3, ndims(data.mom_phase_3):-1:1);  
+end
+
 end
--- lib/power_spectrum.m
+++ lib/power_spectrum.m
@@ -1,11 +1,12 @@
-import_h5(th_spgpe_spectral,'')
+data = import_h5('th_spgpe_spectral','/Users/kieran/Documents/data/chi_0.0562.omega_0.0696.k0_1.16.tl_50.0_v2');
+t_1 = data.t_1;
+herm_dens_2 = data.herm_dens_2;
 figure(1001);
-hold on
+subplot(1,2,1)
 plot(t_1(12:end)-t_1(12),herm_dens_2(12:end,1))
 xlabel('t')
 ylabel('|a_0|^2')
-figure(9909);
-hold on
+subplot(1,2,2)
 Y = fft(herm_dens_2(12:end,1));
 L = length(t_1(12:end));
 P2 = abs(Y/L);
@@ -13,4 +14,7 @@ P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 Fs = 1/(t_1(14)-t_1(13));
 f = Fs*(0:(L/2))/L;
-plot(f/0.0696,P1)
+plot(f/0.0696,P1*100)
+ylim([0 40])
+ylabel('F[|a_0|^2] \times 10^{-2}')
+xlabel('\omega')
