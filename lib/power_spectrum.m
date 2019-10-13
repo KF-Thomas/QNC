51eb6aab54dc43f55386924b7e35e8586615f140
KF-Thomas <35283813+KF-Thomas@users.noreply.github.com>
2019-10-13 19:47:47 +1100

    power spectrum

--- lib/power_spectrum.m
+++ lib/power_spectrum.m
@@ -1,4 +1,4 @@
-data = import_h5('th_spgpe_spectral','/Users/kieran/Documents/data/chi_0.0562.omega_0.0696.k0_1.16.tl_50.0_v2');
+data = import_h5('th_spgpe_spectral','/Users/kieran/Documents/data/chi_0.0562.omega_0.0696.k0_3.67.tl_25.0_v1');
 t_1 = data.t_1;
 herm_dens_2 = data.herm_dens_2;
 figure(1001);
@@ -15,6 +15,6 @@ P1(2:end-1) = 2*P1(2:end-1);
 Fs = 1/(t_1(14)-t_1(13));
 f = Fs*(0:(L/2))/L;
 plot(f/0.0696,P1*100)
-ylim([0 40])
+ylim([0 4])
 ylabel('F[|a_0|^2] \times 10^{-2}')
 xlabel('\omega')
