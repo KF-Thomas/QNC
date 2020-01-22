d56416c50ac59d9099db49e977a86c4611bb6bb7
KF-Thomas <35283813+KF-Thomas@users.noreply.github.com>
2020-01-22 12:59:27 +1100

    bland sim

--- lib/power_spectrum.m
+++ lib/power_spectrum.m
@@ -14,7 +14,26 @@ P1 = P2(1:L/2+1);
 P1(2:end-1) = 2*P1(2:end-1);
 Fs = 1/(t_1(14)-t_1(13));
 f = Fs*(0:(L/2))/L;
-plot(f/0.0696,P1*100)
+plot(f*pi/0.0696,P1*100)
 ylim([0 4])
 ylabel('F[|a_0|^2] \times 10^{-2}')
 xlabel('\omega')
+%%
+figure(465);
+clf
+Y = fft(herm_dens_2(12:end,:));
+L = length(t_1(12:end));
+P2 = abs(Y./L);
+P1 = P2(1:L/2+1,:);
+P1(2:end-1,:) = 2*P1(2:end-1,:);
+Fs = 1/(t_1(14)-t_1(13));
+f = Fs*(0:(L/2))/L;
+j = 0:(size(herm_dens_2(1,:),2)-1);
+pcolor(f*pi/0.0696,j,P1'*100)
+title('F[|a_j|^2] \times 10^{-2}')
+ylabel('a_j')
+xlabel('\omega')
+caxis([0 4])
+colormap(gca,'jet')
+colorbar
+shading flat
--- lib/result_plotter.m
+++ lib/result_plotter.m
@@ -1,5 +1,14 @@
-% clear all
-% th_spgpe_spectral
+clear all
+data = import_h5('th_spgpe_spectral','/Users/kieran/Documents/data/chi_0.0562.omega_0.0696.k0_1.16.tl_50.0_v2');
+t_1 = data.t_1;
+t_2 = data.t_2;
+t_3 = data.t_3;
+x_1 = data.x_1;
+kx_3 = data.kx_3;
+nx_2 = data.nx_2;
+pos_dens_1 = data.pos_dens_1;
+herm_dens_2 = data.herm_dens_2;
+mom_dens_3 = data.mom_dens_3;
 h=0.5;
 omega=0.0696;
 figure(108);
@@ -38,3 +47,34 @@ caxis([0 0.03*max(herm_dens_2,[],'all')])
 % ylabel('k')
 % colormap hot
 % caxis([0 0.06*max(mom_dens_3'.*kx_3.^2,[],'all')])
+%%
+t_indx = t_1<(5.*(2*pi)/omega+t_1(11));
+
+figure(110);
+subplot(1,3,1)
+pcolor((t_1(t_indx)-t_1(11)).*omega/(2*pi),x_1,pos_dens_1(t_indx,:)')
+shading flat
+xlabel('t \omega/(2\pi)')
+ylabel('x')
+title('position')
+colorbar
+colormap(gca,'default')
+caxis([0 h*max(pos_dens_1,[],'all')])
+subplot(1,3,2)
+pcolor((t_3(t_indx)-t_3(11)).*omega/(2*pi),kx_3,mom_dens_3(t_indx,:)')
+shading flat
+xlabel('t \omega/(2\pi)')
+ylabel('k')
+title('momentum')
+colorbar
+colormap(gca,'hot')
+caxis([0 0.1*max(mom_dens_3,[],'all')])
+subplot(1,3,3)
+pcolor((t_2(t_indx)-t_2(11)).*omega/(2*pi),nx_2,herm_dens_2(t_indx,:)')
+shading flat
+xlabel('t \omega/(2\pi)')
+ylabel('a_j')
+title('hermite-gauss')
+colormap(gca,'jet')
+colorbar
+caxis([0 0.03*max(herm_dens_2,[],'all')])
